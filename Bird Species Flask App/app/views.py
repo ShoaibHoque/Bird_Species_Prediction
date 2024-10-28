86829a481ebb2a0bf66fd6a988b2eb0b914e6e25
Shoaib Hoque <shoaibhoque@gmail.com>
2024-10-28 18:52:29 +0600

    updated index and flask app

---
+++ Bird Species Flask App/app/views.py
@@ -0,0 +1,49 @@
+from app import app
+from flask import request, render_template
+from keras import models
+import numpy as np
+from PIL import Image
+import string
+import random
+import os
+
+app.config['INITIAL_FILE_UPLOADS'] = 'static/uploads'
+
+# loading model
+model = models.load_model('static/model/bird_species_model.keras')
+
+# Route to home page
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    # Execute if request is get
+    if request.method == "GET":
+        full_filename = 'images/white_bg.jpg'
+        return render_template("index.html", full_filename = full_filename)
+    
+    # Execute if request id post
+    if request.method == "POST":
+
+        # Generating unique image name
+        letters = string.ascii_lowercase
+        name = ''.join(random.choice(letters) for i in range(10)) + '.png'
+        full_filename = 'uploads/' + name
+
+        # Reading, resizing, sving & preprocessing image
+        image_upload = request.files['image_upload']
+        imagename = image_upload.filename
+        image = Image.open(image_upload)
+        image = image.resize((224,224))
+        image.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], name))
+        image_arr = np.array(image.convert('RGB'))
+        image_arr.shape = (1,224,224,3)
+
+        # predicting output
+        result = model.predict(image_arr)
+        ind = np.argmax(result)
+        classes = ['AMERICAN GOLDFINCH', 'BARN OWL', 'CARMINE BEE-EATER', 'DOWNY WOODPECKER', 'EMPEROR PENGUIN', 'FLAMINGO']
+
+        return render_template('index.html', full_filename = full_filename, pred = classes[ind])
+
+if __name__ == '__main__':
+    app.run(debug=True)
