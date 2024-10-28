254c8c80e8a701dcfe1797048153dda20185b5c4
Shoaib Hoque <shoaibhoque@gmail.com>
2024-10-28 21:44:27 +0600

    predicted image correctly

--- Bird Species Flask App/app/views.py
+++ Bird Species Flask App/app/views.py
@@ -7,10 +7,14 @@ import string
 import random
 import os
 
-app.config['INITIAL_FILE_UPLOADS'] = 'static/uploads'
+# Configure initial file upload path
+app.config['INITIAL_FILE_UPLOADS'] = os.path.join(app.root_path, 'static/uploads')
+
+# Ensure the uploads directory exists
+os.makedirs(app.config['INITIAL_FILE_UPLOADS'], exist_ok=True)
 
 # loading model
-model = models.load_model('static/model/bird_species_model.keras')
+model = models.load_model("app/static/model/bird_species_model.keras")
 
 # Route to home page
 @app.route("/", methods=["GET", "POST"])
@@ -19,31 +23,33 @@ def index():
     # Execute if request is get
     if request.method == "GET":
         full_filename = 'images/white_bg.jpg'
-        return render_template("index.html", full_filename = full_filename)
+        return render_template("index.html", full_filename=full_filename)
     
-    # Execute if request id post
+    # Execute if request is post
     if request.method == "POST":
+
+        # Ensure the uploads directory exists
+        os.makedirs(app.config['INITIAL_FILE_UPLOADS'], exist_ok=True)
 
         # Generating unique image name
         letters = string.ascii_lowercase
         name = ''.join(random.choice(letters) for i in range(10)) + '.png'
         full_filename = 'uploads/' + name
 
-        # Reading, resizing, sving & preprocessing image
+        # Reading, resizing, saving & preprocessing image
         image_upload = request.files['image_upload']
-        imagename = image_upload.filename
         image = Image.open(image_upload)
         image = image.resize((224,224))
         image.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], name))
         image_arr = np.array(image.convert('RGB'))
         image_arr.shape = (1,224,224,3)
 
-        # predicting output
+        # Predicting output
         result = model.predict(image_arr)
         ind = np.argmax(result)
         classes = ['AMERICAN GOLDFINCH', 'BARN OWL', 'CARMINE BEE-EATER', 'DOWNY WOODPECKER', 'EMPEROR PENGUIN', 'FLAMINGO']
 
-        return render_template('index.html', full_filename = full_filename, pred = classes[ind])
+        return render_template('index.html', full_filename=full_filename, pred=classes[ind])
 
 if __name__ == '__main__':
     app.run(debug=True)
